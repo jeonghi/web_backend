d440afced3c6442295a0bf4727dc3d9fdb7850c8
jeonghi <jpark0902@kookmin.ac.kr>
2022-07-27 17:50:21 +0900

    추가 : create Todo(생성기능) , retrieve Todo(검색기능) 리포지토리, 서비스, 컨트롤러구현

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -2,30 +2,78 @@ package com.example.demo.controller;
 
 
 import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TodoDTO;
+import com.example.demo.model.TodoEntity;
 import com.example.demo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.xml.ws.Response;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("todo")
 public class TodoController {
 
     @Autowired
-    private TodoService todoService;
+    private TodoService service;
 
     @GetMapping("/test")
     public ResponseEntity<?> testTodo() {
-        String str = todoService.testService();
+        String str = service.testService();
         List<String> list = new ArrayList<>();
         list.add(str);
         ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto){
+        try{
+            String tempUserId = "temporary-user";
+
+            // 1. TodoEntity 로 변환한다.
+            TodoEntity entity = TodoDTO.toEntity(dto);
+
+            // 2. id를 Null로 초기화 한다. 생성 당시에는 id가 없어야 하기 때문이다.
+            entity.setId(null);
+
+            // 3. 임시 사용자 아이디를 설정해 준다. 이 부분은 인증과 인가에서 수정할 예정.
+            // 지금은 인증 기능이 없으므로 오직 한 사용자만 로그인 없이 사용할 수 있는 어플리케이션인 셈이다.
+            entity.setUserId(tempUserId);
+
+            // 4. 서비스를 이용해 Todo 엔티티를 생성한다.
+            List<TodoEntity> entities = service.create(entity);
+
+            // 5. 자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환한다.
+            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+            // 6. 변환된 TodoDTO 리스트를 이용해 ResponseDTO를 초기화한다.
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+            return ResponseEntity.ok().body(response);
+        }catch (Exception e){
+            String error = e.getMessage();
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> retrieveTodoList() {
+        String tempUserId = "temporary-user"; // temporary user id.
+
+        // 1. 서비스 메서드의 retrieve() 메서드를 사용해서 Todo 리스트를 가져온다.
+        List<TodoEntity> entities = service.retrieve(tempUserId);
+        // 2. 자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환한다.
+        List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+        // 3. 변환된 TodoDTO 리스트를 이용해 ResponseDTO 를 초기화한다.
+        ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+        // 4. ResponseDTO를 리턴한다.
         return ResponseEntity.ok().body(response);
     }
 }
--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -2,9 +2,21 @@ package com.example.demo.service;
 
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+
+// 어떤 로그는 정보를 위한 것이고, 어떤 로그는 디버깅을 위한 자세한 정보일 수 있으며, 또 일부 로그는 심각한 에러를 알려주는 로그일 수도 있다.
+// 이렇게 용도에 따라 로그를 크게 info, debug, warn, error 로 나누고 이를 로그 레벨이라고 부른다.
+// 우리에겐 이미 이런 기능을 제공하는 slf4j라는 라이브러리가 존재한다.
+// 로깅은 웹 서비스에서 반드시 필요하다 !
+// slf4j는 로그계의 JPA 정도로 생각하면 된다. slf4j를 사용하려면 구현부를 연결해줘야한다.
+// 이 연결 작업은 스프링이 알아서 해준다.
+// 스프링은 기본적으로 Logback 로그 라이브러리를 사용한다.
+@Slf4j // 로그 라이브러리를 사용하려면 로깅할 대상 클래스 위에 다음과 같은 어노테이션을 추가하면 된다.
 @Service
 public class TodoService {
 
@@ -19,5 +31,36 @@ public class TodoService {
         TodoEntity savedEntity = repository.findById(entity.getId()).get(); // findById의 반환값이 Optional<T>임 . 옵셔널에는 종단 처리로 get 이 존재함.
 
         return savedEntity.getTitle();
+    }
+
+    public List<TodoEntity> create(final TodoEntity entity){
+        // 1. 검증
+        // 넘어온 엔티티가 유효한지 검사하는 로직으로 이 부분은 코드가 더 커지면 TodoValidator.java로 분리시킬 수 있다.
+        validate(entity);
+        // 2. save()
+        // 엔티티를 데이터베이스에 저장하고 로그를 남긴다.
+        repository.save(entity);
+        log.info("Entity Id : {} is saved", entity.getId());
+
+        // 3. findByUserId()
+        // 저장된 엔티티를 포함하는 새 리스트를 리턴한다.
+        return repository.findByUserId(entity.getUserId());
+    }
+
+    public List<TodoEntity> retrieve(final String userId){
+        return repository.findByUserId(userId);
+    }
+
+    private void validate(TodoEntity entity) {
+        if(entity == null){
+            log.warn("Entity cannot be null.");
+            throw new RuntimeException("Entity cannot be null");
+        }
+
+
+        if(entity.getUserId() == null){
+            log.warn("Unknown User.");
+            throw new RuntimeException("Unknown User.");
+        }
     }
 }
